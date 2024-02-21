79fd5f129c0ad0c521cf9911149b37141ec0e28f
Ramon Pietro <oramonroque@outlook.com>
2024-02-21 01:26:48 -0300

    aula IV

---
+++ Avaliação I/Aula IV/figuras geometricas/polygon.py
@@ -0,0 +1,20 @@
+import pygame
+import sys
+
+pygame.init()
+
+window = pygame.display.set_mode((600, 500))
+
+black = 0, 0, 0
+blue = (0, 0, 255)
+white = 255, 255, 255
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    window.fill(black)
+
+    pygame.draw.polygon(window, blue, [(150, 300), (300, 100), (450, 300)], 0)
+
+    pygame.display.flip()
